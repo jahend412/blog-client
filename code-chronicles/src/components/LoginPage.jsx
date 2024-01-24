1ea44b5f6b5fce463e302f578068829f593d77dc
Josh Endemann <jahend44@gmail.com>
2024-01-23 20:00:12 -0600

    Create LoginPage.jsx
    Ending the Night with the start of the Login Page.

---
+++ code-chronicles/src/components/LoginPage.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function LoginPage() {
+    <form action="">
+        <input type="text" />
+        <input type="password" />
+        <button>Login</button>
+    </form>
+};
