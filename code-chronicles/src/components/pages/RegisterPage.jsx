44c9e0f35adf29861e92a29c0e35b15eea3e6702
Josh Endemann <jahend44@gmail.com>
2024-01-28 18:04:17 -0600

    Update RegisterPage.jsx
    In this update :
    
    1.  We import useState hook from react.  This will manage state variables in the component.
    2.  We use the useState hook to declare state varaiables username and password.
    3.  We declare an asynchronus function named register that handles the registration process.
    4.  I then called the event.preventDefault() to prevent the default form submission behaviaor and use fetch function to send a POST request to my api localhost.
    5.  Finally I set the value and the onChange for both the username and password.

--- code-chronicles/src/components/pages/RegisterPage.jsx
+++ code-chronicles/src/components/pages/RegisterPage.jsx
@@ -1,17 +1,34 @@
-import React from "react";
+import { useState } from "react";
+
 
 export default function RegisterPage() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    async function register(event) {
+        event.preventDefault();
+        fetch('http://localhost:4000', {
+            method: 'Post',
+            body: JSON.stringify({ username, password }),
+            headers: { 'Content-Type': 'application/json' }
+        })
+    }
     return (
         <div>
             <h2>Code Chronicles</h2>
-            <form className="register">
+            <form className="register" onSubmit={register}>
                 <h1>Register</h1>
                 <input
                     type="text"
-                    placeholder="Username" />
+                    placeholder="Username"
+                    value={username}
+                    onChange={event => setUsername(event.target.value)}
+                />
                 <input
                     type="password"
-                    placeholder="Password" />
+                    placeholder="Password"
+                    value={password}
+                    onChange={event => setPassword(event.target.value)}
+                />
                 <button>Register</button>
             </form>
         </div>
