5f1c4be167dddb761d34bdcffd51b07b00a72138
Josh Endemann <jahend44@gmail.com>
2024-01-30 19:59:59 -0600

    Update LoginPage.jsx
    In this update:
    
    First I am using State Management 'useState' hook to manage the state for username and password.
    
    Then in the login function I am sending a POST request to the login endpoint.
    
    Finally I add the value and onChange attributes to bind the input fields to the state, ensuring the user interface  reflects the current state of the form.

--- code-chronicles/src/components/pages/LoginPage.jsx
+++ code-chronicles/src/components/pages/LoginPage.jsx
@@ -1,17 +1,38 @@
-import React from "react";
+import { useState } from "react";
+
 
 export default function LoginPage() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    async function login(ev) {
+        ev.preventDefault();
+        await fetch('http://localhost:4000/login', {
+            method: 'POST',
+            body: JSON.stringify({ username, password }),
+            headers: { 'Content-Type': 'application/json' },
+        })
+    }
     return (
         <div>
             <h2>Code Chronicles</h2>
-            <form className="login">
+            <form
+                className="login"
+                onSubmit={login}
+            >
+
                 <h1>Login</h1>
                 <input
                     type="text"
-                    placeholder="Username" />
+                    placeholder="Username"
+                    value={username}
+                    onChange={ev => setUsername(ev.target.value)}
+                />
                 <input
                     type="password"
-                    placeholder="Password" />
+                    placeholder="Password"
+                    value={password}
+                    onChange={ev => setPassword(ev.target.value)}
+                />
                 <button>Login</button>
             </form>
         </div>
