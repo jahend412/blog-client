ef1ee78cb016f5ede063cd25ea3e739243d058b6
Josh Endemann <jahend44@gmail.com>
2024-01-31 19:35:19 -0600

    Update NavBar.jsx
    Cleaned up code first.  Then added functionality to the navbar to allow it to show login and register when a user is not logged in and the username and userInfo if the user is logged into the navbar.

--- code-chronicles/src/components/NavBar.jsx
+++ code-chronicles/src/components/NavBar.jsx
@@ -1,20 +1,46 @@
 // Navbar.js
 import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
-import React from 'react';
+export default function Navbar() {
+    const [username, setUsername] = useState(null);
+    useEffect(() => {
+        fetch('http://localhost:4000/profile', {
+            credentials: 'include',
+        }).then(response => {
+            response.json().then(userInfo => {
+                setUsername(userInfo, username)
+            });
+        });
+    }, []);
 
-const Navbar = () => {
+    function logout() {
+        fetch('http://localhost:4000/logout', {
+            credentials: 'include',
+            method: 'POST',
+        });
+
+    }
+
     return (
         <header>
-            <Link to="/">Code Chronicles</Link>
+            <Link to="/" className="logo">Code Chronicles</Link>
             <nav>
-                <Link to="/login">Login </Link>
-                <Link to="/register">Register</Link>
-
+                {username && (
+                    <>
+                        <Link to="/create">Create new post</Link>
+                        <a onClick={logout}>Logout ({username})</a>
+                    </>
+                )}
+                {!username && (
+                    <>
+                        <Link to="/login">Login</Link>
+                        <Link to="/register">Register</Link>
+                    </>
+                )}
             </nav>
         </header>
 
     );
 };
 
-export default Navbar;
