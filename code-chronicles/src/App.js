2442ee6211873f54eb4ff50aacbb4a89241e44f8
Josh Endemann <jahend44@gmail.com>
2024-02-01 19:32:56 -0600

    Update App.js
    Fixed typo on ending UserContextProvider

--- code-chronicles/src/App.js
+++ code-chronicles/src/App.js
@@ -5,17 +5,19 @@ import LoginPage from './components/pages/LoginPage';
 import Layout from './components/Layout';
 import RegisterPage from './components/pages/RegisterPage';
 import HomePage from './components/pages/HomePage';
+import { UserContextProvider } from './UserContext';
 
-export default function App() {
+function App() {
   return (
-    <Routes>
-      <Route path='/' element={<Layout />}>
-        <Route index element={<HomePage />} />
-        <Route path={'/login'} element={<LoginPage />} />
-        <Route path={'/register'} element={<RegisterPage />} />
-      </Route>
-    </Routes>
-
-  );
+    <UserContextProvider>
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route index element={<HomePage />} />
+          <Route path={'/login'} element={<LoginPage />} />
+          <Route path={'/register'} element={<RegisterPage />} />
+        </Route>
+      </Routes>
+    </UserContextProvider>);
 };
 
+export default App;
