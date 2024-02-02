62abda34520461c9439badbf29261a2fdaff770d
Josh Endemann <jahend44@gmail.com>
2024-02-01 19:21:47 -0600

    Update UserContext.jsx
    In this file:
    
    I am importing createContext function from the React library.
    
    Next I am creating a new context called UserContext using createContext.
    
    The UserContextProvider is a provider for the UserContext and takes a children prop to represent the component that will be wrapped.

--- code-chronicles/src/UserContext.jsx
+++ code-chronicles/src/UserContext.jsx
@@ -1,3 +1,12 @@
 import { createContext } from "react";
 
 export const UserContext = createContext({});
+
+export function UserContextProvider({ children }) {
+    const [userInfo, setUserInfo] = useState(null);
+    return (
+        <UserContextProvider value={{ userInfo, setUserInfo }} >
+            {children}
+        </UserContextProvider>
+    );
+}
