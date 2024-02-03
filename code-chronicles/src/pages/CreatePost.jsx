283df5cea2ac72a696f975e3f8132d297be26a98
Josh Endemann <jahend44@gmail.com>
2024-02-02 20:57:20 -0600

    Update CreatePost.jsx
    I have moved const modules and const formats above my CreatePost function.
    
    I have also added value and onchange properties to my inputs and ReactQuill to make use of the useState hook

--- code-chronicles/src/pages/CreatePost.jsx
+++ code-chronicles/src/pages/CreatePost.jsx
@@ -2,29 +2,51 @@ import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 import { useState } from 'react';
 
+const modules = {
+    toolbar: [
+        [{ 'header': [1, 2, false] }],
+        ['bold', 'italic', 'underline', 'strike'],
+        [{ 'list': 'ordered' }, { 'list': 'bullet' }], [{ 'indent': '-1' }, { 'indent': '+1' }], ,
+        ['link', 'image'],
+        ['clean']
+    ]
+};
+const formats = [
+    'header',
+    'bold', 'italic', 'underline', 'strike', 'blockquote',
+    'list', 'bullet', 'indent',
+    'link', 'image'
+];
+
 export default function CreatePost() {
     const [title, setTitle] = useState('');
     const [summary, setSummary] = useState('');
     const [content, setContent] = useState('');
-    const modules = {
-        toolbar: [
-            [{ 'header': [1, 2, false] }],
-            ['bold', 'italic', 'underline', 'strike'],
-            [{ 'list': 'ordered' }, { 'list': 'bullet' }], [{ 'indent': '-1' }, { 'indent': '+1' }], ,
-            ['link', 'image'],
-            ['clean']
-        ]
-    };
 
     return (
         <div className="create-post">
             <h1>Create a new post</h1>
             <form>
 
-                <input type="title" placeholder="Title" />
-                <input type="summary" placeholder='summary' ></input>
-                <input type="file" />
-                <ReactQuill value={content} modules={modules} />
+                <input type="title"
+                    placeholder="Title"
+                    value={title}
+                    onChange={event => setTitle(event.target.value)}
+                />
+                <input
+                    type="summary"
+                    placeholder='summary'
+                    value={summary}
+                    onChange={event => setSummary(event.target.value)}
+                />
+                <input type="file"
+
+                />
+                <ReactQuill
+                    value={content}
+                    onChange={newContent => setContent(newContent)}
+                    odules={modules}
+                    formats={formats} />
                 <button type="submit" style={{ marginTop: '5px' }}>Create Post</button>
             </form>
         </div>
