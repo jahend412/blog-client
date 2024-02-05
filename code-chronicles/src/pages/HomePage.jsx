522f32bbfd6f70530302491c0c3dd230c78c21d6
Josh Endemann <jahend44@gmail.com>
2024-02-04 19:03:53 -0600

    Update HomePage.jsx
    In this update:
    
    I am using the useState and useEffect hooks to fetch and display posts from the server.   These hooks manage the state of the posts and trigger the data fetching process.
    
    At the end I use conditonal rendering check if there are posts in the 'posts' array.  This will map over the array and renders individual 'Post' components , spreading the properties of each post as props.

--- code-chronicles/src/pages/HomePage.jsx
+++ code-chronicles/src/pages/HomePage.jsx
@@ -1,12 +1,22 @@
+import { useEffect, useState } from "react";
 import Post from "../Post";
 
 
 export default function HomePage() {
+    const [posts, setPosts] = useState([]);
+    useEffect(() => {
+        const response = fetch('http://localhost:4000/post').then(response => {
+            response.json().then(posts => {
+                setPosts(posts);
+            });
+        })
+    }, []);
+
     return (
         <>
-            <Post />
-            <Post />
-            <Post />
+            {posts.length > 0 && posts.map((post => (
+                <Post {...post} />
+            )))}
         </>
     );
 };
